4b2031081310c087e748ee9797769a2b6b6a0522
Luiz Verissimo <luizverissimoccr@hotmail.com>
2016-08-17 13:44:17 -0300

    começando layout

--- src/banco/TestaConta.java
+++ src/banco/TestaConta.java
@@ -1,11 +1,30 @@
 package banco;
 
-import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class TestaConta {
 
 	public static void main(String[] args) {
-		
+		short opcaoMenu = -1;
+		Scanner dadoEscolhido = new Scanner(System.in);
+		do {
+			System.out.print(" ---Banco Ficticio---\n"
+					         + "| 1 - Criar Conta    |"
+					         + "\n| 2 - Ver saldo      |"
+					         + "\n| 3 - Depositar      |"
+					         + "\n| 4 - Sacar          |"
+					         + "\n ---------------------"
+					         + "\nInsira a opção: ");
+			opcaoMenu = dadoEscolhido.nextShort();
+			switch (opcaoMenu) {
+			case 1:
+				
+				break;
+
+			default:
+				break;
+			}
+		} while (opcaoMenu != 0);
 	}
 	
 }
